88729f5c866eebf827f2e6412409a8be38f4d874
jrauljperez02 <raul_jimenez_71@outloo.es>
2022-07-02 09:12:55 -0500

    Set token header using ModHeader extension

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from rest_framework import filters  
 
@@ -117,3 +119,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     #filter elements in our api
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email','id',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
